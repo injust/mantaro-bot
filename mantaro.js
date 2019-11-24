300a694f6c1ffc2404fdf2002d1123729385967b
Justin Su <injustsu@gmail.com>
2019-11-24 14:12:41 -0500

    users => workers

--- mantaro.js
+++ mantaro.js
@@ -6,56 +6,55 @@ const { lookup } = require("./lookup.js");
 /***********
  * EXPORTS *
  ***********/
-
 exports.daily = () => {
-	const userKeys = Object.keys(config.users);
-	return Promise.all(userKeys.map((user) => send(config.users[user].channel, user, `->daily <@${config.users[userKeys[user === userKeys[0] && userKeys.length > 1 ? 1 : 0]].id}>`)));
+	const workers = Object.keys(config.workers);
+	return Promise.all(workers.map((worker) => send(config.workers[worker].channel, worker, `->daily <@${config.workers[workers[worker === workers[0] && workers.length > 1 ? 1 : 0]].id}>`)));
 };
 
 exports.quiz = () => {
-	const userKeys = Object.keys(config.users);
+	const workers = Object.keys(config.workers);
 	const jobs = [];
-	const numJobs = Math.trunc(userKeys.length / config.workersPerJob);
+	const numJobs = Math.trunc(workers.length / config.workersPerJob);
 	for (let j = 0; j < numJobs; j++) {
 		for (let w = 0; w < config.workersPerJob; w++) {
-			jobs.push(sleep(config.intervals.send * (j / numJobs + w / config.workersPerJob)).then(() => run(config.users[userKeys[j * config.workersPerJob + w]].channel, userKeys[j * config.workersPerJob + w], userKeys[j * config.workersPerJob])));
+			jobs.push(sleep(config.intervals.send * (j / numJobs + w / config.workersPerJob)).then(() => run(config.workers[workers[j * config.workersPerJob + w]].channel, workers[j * config.workersPerJob + w], workers[j * config.workersPerJob])));
 		}
 	}
 	return Promise.all(jobs);
 };
 
-exports.rep = () => allSend(`->rep <@${config.users[Object.keys(config.users)[0]].id}>`);
+exports.rep = () => allSend(`->rep <@${config.workers[Object.keys(config.workers)[0]].id}>`);
 
 exports.sweep = () => {
-	const userKeys = Object.keys(config.users);
-	return Promise.all(userKeys.map((user, i) => sleep(i * config.intervals.sweep / userKeys.length).then(() => sweepUser(config.users[user].channel, user, userKeys[0]))));
+	const workers = Object.keys(config.workers);
+	return Promise.all(workers.map((worker, i) => sleep(i * config.intervals.sweep / workers.length).then(() => sweepUser(config.workers[worker].channel, worker, workers[0]))));
 };
 
-exports.waifu = () => allSend(`->waifu claim <@${config.users[Object.keys(config.users)[0]].id}>`);
+exports.waifu = () => allSend(`->waifu claim <@${config.workers[Object.keys(config.workers)[0]].id}>`);
 
 /*******
  * API *
  *******/
 const allSend = (msg) => {
-	return Promise.all(Object.keys(config.users).map((user) => send(config.users[user].channel, user, msg)));
+	return Promise.all(Object.keys(config.workers).map((worker) => send(config.workers[worker].channel, worker, msg)));
 };
 
 const api = "https://discordapp.com/api/v6";
 
-const get = (channel, user, filter) => fetch(`${api}/channels/${config.channels[channel].id}/messages?limit=5`, {
+const get = (channel, worker, filter) => fetch(`${api}/channels/${config.channels[channel].id}/messages?limit=5`, {
 	headers: {
-		Authorization: config.users[user].token
+		Authorization: config.workers[worker].token
 	}
 }).then((response) => response.json()).then((messages) => messages.find(filter));
 
 const mantaroID = "213466096718708737";
 
-const send = (channel, user, msg) => {
-	console.log(`@${user} in #${channel}: ${msg}`);
+const send = (channel, worker, msg) => {
+	console.log(`@${worker} in #${channel}: ${msg}`);
 	return fetch(`${api}/channels/${config.channels[channel].id}/messages`, {
 		body: `{"content": "${msg}"}`,
 		headers: {
-			Authorization: config.users[user].token,
+			Authorization: config.workers[worker].token,
 			"Content-Type": "application/json"
 		},
 		method: "POST"
@@ -65,18 +64,18 @@ const send = (channel, user, msg) => {
 /****************
  * ITEM SWEEPER *
  ****************/
-const sweepUser = async (channel, user, primary) => {
-	if (primary === user) {
+const sweepUser = async (channel, worker, primary) => {
+	if (primary === worker) {
 		return;
 	}
-	await send(channel, user, "->inv");
+	await send(channel, worker, "->inv");
 	await sleep(config.intervals.send);
-	await get(channel, user, (msg) => msg.author.id === mantaroID && msg.content.includes("'s inventory:** ")).then(async (msg) => {
+	await get(channel, worker, (msg) => msg.author.id === mantaroID && msg.content.includes("'s inventory:** ")).then(async (msg) => {
 		const inventory = msg.content.split("\n")[0].split("** ")[1].split(", ");
 		for (const item of inventory) {
 			[name, quantity] = item.split(" x ");
 			if (name !== "💾" && name !== "⛏" && name !== "🎣" && !name.includes("lootbox:")) {
-				await send(channel, user, `->itemtransfer <@${config.users[primary].id}> ${name} ${quantity}`);
+				await send(channel, worker, `->itemtransfer <@${config.workers[primary].id}> ${name} ${quantity}`);
 				await sleep(config.intervals.sweep);
 			}
 		}
@@ -94,18 +93,18 @@ const answer = (msg) => {
 	return answers.indexOf(lookup[question]) + 1;
 };
 
-const run = async (channel, user, primary) => {
-	await send(channel, user, "->opts lobby reset");
+const run = async (channel, worker, primary) => {
+	await send(channel, worker, "->opts lobby reset");
 	await sleep(config.intervals.send);
 	for (let i = 0;; i++) {
 		if (i % config.modulo < 3) {
-			await send(channel, user, actions[i % config.modulo]);
+			await send(channel, worker, actions[i % config.modulo]);
 			await sleep(config.intervals.send);
 		}
-		await send(channel, user, `->game multiple trivia <@${config.users[primary].id}> 5 -diff hard`);
+		await send(channel, worker, `->game multiple trivia <@${config.workers[primary].id}> 5 -diff hard`);
 		await sleep(config.intervals.send);
 		for (let j = 0; j < 5; j++) {
-			await get(channel, user, (msg) => msg.author.id === mantaroID && msg.embeds.length > 0 && msg.embeds[0].author.name === "Trivia Game").then((msg) => send(channel, primary, answer(msg)));
+			await get(channel, worker, (msg) => msg.author.id === mantaroID && msg.embeds.length > 0 && msg.embeds[0].author.name === "Trivia Game").then((msg) => send(channel, primary, answer(msg)));
 			await sleep(config.intervals.send);
 		}
 	}
