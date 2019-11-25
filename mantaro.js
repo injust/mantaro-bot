7af024c300059281d416a9f653877c94cf6c9e61
Justin Su <injustsu@gmail.com>
2019-11-25 04:48:43 -0500

    Safer message retrieval

--- mantaro.js
+++ mantaro.js
@@ -35,7 +35,7 @@ const client = new Discord.Client({
 client.login(config.bots.listener.token);
 
 client.on("message", (msg) => {
-	if (msg.author.id === config.bots.mantaro.id && (msg.content.includes("'s inventory:** ") || msg.embeds.length > 0 && msg.embeds[0].author.name === "Trivia Game")) {
+	if (msg.author.id === config.bots.mantaro.id && (msg.content[0] === "✖" || msg.embeds.length > 0 && msg.embeds[0].author.name === "Trivia Game") || msg.content.includes("'s inventory:** ")) {
 		messages[msg.channel.name] = msg;
 	}
 });
@@ -53,12 +53,14 @@ const sweepUser = async (channel, worker, primary) => {
 	}
 	await send(channel, worker, "->inv");
 	await sleep(config.intervals.send);
-	const inventory = messages[channel].content.split("\n")[0].split("** ")[1].split(", ");
-	for (const item of inventory) {
-		[name, quantity] = item.split(" x ");
-		if (name !== "💾" && name !== "⛏" && name !== "🎣" && !name.includes("lootbox:")) {
-			await send(channel, worker, `->itemtransfer <@${config.workers[primary].id}> ${name} ${quantity}`);
-			await sleep(config.intervals.sweep);
+	if (messages[channel] != null && msg.content.includes("'s inventory:** ")) {
+		const inventory = messages[channel].content.split("\n")[0].split("** ")[1].split(", ");
+		for (const item of inventory) {
+			[name, quantity] = item.split(" x ");
+			if (name !== "💾" && name !== "⛏" && name !== "🎣" && !name.includes("lootbox:")) {
+				await send(channel, worker, `->itemtransfer <@${config.workers[primary].id}> ${name} ${quantity}`);
+				await sleep(config.intervals.sweep);
+			}
 		}
 	}
 };
@@ -75,17 +77,21 @@ const answer = (msg) => {
 };
 
 const run = async (channel, worker, primary) => {
-	await send(channel, worker, "->opts lobby reset");
-	await sleep(config.intervals.send);
 	for (let i = 0;; i++) {
 		if (i % config.modulo < 3) {
 			await send(channel, worker, actions[i % config.modulo]);
 			await sleep(config.intervals.send);
 		}
+		if (messages[channel] != null && messages[channel][0] === "✖") {
+			await send(channel, worker, "->opts lobby reset");
+			await sleep(config.intervals.send);
+		}
 		await send(channel, worker, `->game multiple trivia <@${config.workers[primary].id}> 5 -diff hard`);
 		await sleep(config.intervals.send);
 		for (let j = 0; j < 5; j++) {
-			send(channel, primary, answer(messages[channel]));
+			if (messages[channel] != null && messages[channel].embeds.length > 0 && messages[channel].embeds[0].author.name === "Trivia Game") {
+				send(channel, primary, answer(messages[channel]));
+			}
 			await sleep(config.intervals.send);
 		}
 	}
